7ef627831241d0bafb27928bcc05132a2369751c
Rakesh mohan <maheshmohan988@gmail.com>
2021-12-16 11:34:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/views/splash_view.dart';
+import 'package:flutter/services.dart';
 
 import 'firebase_options.dart';
 
